09b98945489257ed21fe136025cb68af5ac5a90e
GokulMs-1045 <gokunavi2004@gmail.com>
2025-03-04 10:15:23 +0530

    admin del

--- ADMIN/BACKEND/routes/adminReqRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const adminreqOrders  = require('../controllers/adminReq');
-
-const router = express.Router();
-
-router.get('/user/:id', adminreqOrders);
-
-module.exports = router;
