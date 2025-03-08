134005ebd77ea3e8e4eafac660b2330284bd6cd0
Deepak2005-06 <deepaksathis2005@gmail.com>
2025-03-08 11:51:23 +0530

    mystore

--- BACKEND/routes/myStoreRoutes.js
+++ BACKEND/routes/myStoreRoutes.js
@@ -1,17 +1,17 @@
 const express = require('express');
-const { getStore, editStore } = require('../controllers/myStore/mystore');
+const { getDealerByGoogleId,updateDealerByGoogleId} = require('../controllers/myStore/mystore.js');
 
 const router = express.Router();
 
 // ✅ Route to get store details
-router.get('/myStore/:googleid', getStore);
+router.get('/myStore/:googleId', getDealerByGoogleId );
 
 // ✅ Route to update store details
-router.put('/myStore/:googleid/edit', editStore);
+router.put('/myStore/:googleId/edit', updateDealerByGoogleId);
 
-const { addProduct, getProductsByGoogleid } = require("../controllers/myStore/storeProducts");
+// const { addProduct, getProductsByGoogleid } = require("../controllers/myStore/storeProducts");
 
-router.post("/products/:phoneNumber", addProduct);
-router.get("/products/:phoneNumber", getProductsByGoogleid);
+// router.post("/products/:phoneNumber", addProduct);
+// router.get("/products/:phoneNumber", getProductsByGoogleid);
 
 module.exports = router;
