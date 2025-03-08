0fa5366087bd2247631b32eae9fa676ae832798c
GokulMs-1045 <gokunavi2004@gmail.com>
2025-03-08 09:35:07 +0530

    delete

--- BACKEND/routes/signupRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const  signupUser  = require('../controllers/signup/signup');
-
-router.post('/signup', signupUser);
-
-module.exports = router;
